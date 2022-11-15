ff4f4062e46d12aa914dc51f48977f6a9eb71867
sasha_litvit <sasha_litvit@mail.ru>
2022-11-15 21:53:57 +0300

    feat: add video

--- README.md
+++
@@ -1,2 +0,0 @@
-# Songbird
-Application
--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const headerBtn = document.querySelector('.header-btn')
+const headerBtnName = document.querySelector('.return-play')
+const mainScreen = document.querySelector('.main-block')
 
+console.log(headerBtn, headerBtnName)
 
+headerBtn.addEventListener('click', ()=>{
+    mainScreen.classList.toggle('view-2')
+    if(mainScreen.classList.contains('view-2')){
+        headerBtnName.innerHTML = 'return'
+    } else{
+        headerBtnName.innerHTML = 'play'
+    }
+    
+})
