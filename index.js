c3caa5c44e27f6e170628678c99a6866fac18b5d
sasha_litvit <sasha_litvit@mail.ru>
2022-11-19 11:33:55 +0300

    feat:add grid container

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import birdsData from "./birds.js"
+
+const body = document.querySelector('body')
 const headerBtn = document.querySelector('.header-btn')
 const headerBtnName = document.querySelector('.return-play')
 const mainScreen = document.querySelector('.main-block')
-
-console.log(headerBtn, headerBtnName)
+const soundBtn = document.querySelector('.sound-btn')
+const video = document.querySelector('video')
 
 headerBtn.addEventListener('click', ()=>{
     mainScreen.classList.toggle('view-2')
@@ -12,4 +15,16 @@ headerBtn.addEventListener('click', ()=>{
         headerBtnName.innerHTML = 'play'
     }
     
+})
+
+console.log(soundBtn)
+soundBtn.classList.toggle('sound-btn_stop')
+soundBtn.addEventListener('click', ()=>{
+    soundBtn.classList.toggle('sound-btn_stop')
+    soundBtn.classList.toggle('sound-btn_active')
+    if (soundBtn.classList.contains('sound-btn_active')){
+        video.muted = false
+    } else {
+        video.muted = true
+    }
 })
