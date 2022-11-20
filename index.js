dd5d5d11c9a2beed3c8efae4099bf70a9969e53e
sasha_litvit <sasha_litvit@mail.ru>
2022-11-20 20:08:34 +0300

    feat: add curc

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const mainScreen = document.querySelector('.main-block')
 const soundBtn = document.querySelector('.sound-btn')
 const video = document.querySelector('video')
 const taskBirdsNames = document.querySelectorAll('.answer li span')
+const taskBirdsNamesMarkers = document.querySelectorAll('.answer li')
 const taskSelection = document.querySelector('.page-2_nav')
 
-let indexBirdsData, nameBird
+
+let indexBirdsData, nameBird,
+objectAnswer = {
+    guess: '',
+    answer:''
+}
 
 preloadImg()
 
@@ -23,7 +29,7 @@ score.classList.toggle('invisible')
 scoreRes.classList.toggle('invisible')
 
 headerBtn.addEventListener('click', ()=>{
-    
+
     selectName()
     mainScreen.classList.toggle('view-2')
 
@@ -34,8 +40,8 @@ headerBtn.addEventListener('click', ()=>{
         scoreRes.classList.toggle('invisible')
 
         if (soundBtn.classList.contains('sound-btn_active')){
-            
-            video.muted = true            
+
+            video.muted = true
             soundBtn.classList.toggle('sound-btn_active')
             soundBtn.classList.toggle('sound-btn_stop')
         }
@@ -45,12 +51,12 @@ headerBtn.addEventListener('click', ()=>{
         scoreRes.classList.toggle('invisible')
 
         video.muted = false
-        soundBtn.classList.toggle('sound-btn_stop')          
+        soundBtn.classList.toggle('sound-btn_stop')
         soundBtn.classList.toggle('sound-btn_active')
-        
+
 
     }
-    
+
 })
 
 // main
@@ -62,7 +68,7 @@ soundBtn.addEventListener('click', ()=>{
     soundBtn.classList.toggle('sound-btn_stop')
     soundBtn.classList.toggle('sound-btn_active')
 
-    toggleScreen()    
+    toggleScreen()
 
     if (soundBtn.classList.contains('sound-btn_active')){
         video.muted = false
@@ -74,8 +80,8 @@ soundBtn.addEventListener('click', ()=>{
 // 2.2 write names of birds
 
     // получаем название группы птиц по кнопке
-taskSelection.addEventListener('click', (event)=>{ 
-        
+taskSelection.addEventListener('click', (event)=>{
+
         switch(event.target.textContent){
             case 'Разминка':
                 indexBirdsData = 0
@@ -95,10 +101,11 @@ taskSelection.addEventListener('click', (event)=>{
             case 'Морские птицы':
                 indexBirdsData = 5
                 break
-            default:                
+            default:
                 return
-        }    
+        }
         selectName(indexBirdsData)
+        checkMarkerClass()
 })
 
 
@@ -106,37 +113,39 @@ taskSelection.addEventListener('click', (event)=>{
 
 // block answer
     // по кнопке в меню, выводим название птиц вблоке задание
-function selectName(index = 0){        
+function selectName(index = 0){
         birdsData[index].forEach((el, i)=>{
             taskBirdsNames[i].textContent = el.name
         })
         if(!index){
             indexBirdsData = 0
         }
+        guessBird(indexBirdsData)
 }
 
 taskBirdsNames.forEach(el =>{
-    
+
     el.addEventListener('click',(event)=>{
-        
+
         nameBird = event.target.textContent
 
         birdsData[indexBirdsData].forEach((el, i) =>{
 
             if(el.name === nameBird){
-                let index = i
+                const index = i
                 descriptionBird(index)
             }
+
         })
-    })    
+    })
 })
 
 // block description
 function descriptionBird(i){
 
     const describeName = document.querySelector('.describe-bird h2')
-    describeName.textContent = `${birdsData[indexBirdsData][i].name}` + '/' + `${birdsData[indexBirdsData][i].species}`   
-    
+    describeName.textContent = `${birdsData[indexBirdsData][i].name}` + '/' + `${birdsData[indexBirdsData][i].species}`
+
     const describeImg = document.querySelector('.describe-bird img')
 
     if(`${birdsData[indexBirdsData][i].name}` === 'Пеликан'){
@@ -145,11 +154,15 @@ function descriptionBird(i){
         describeImg.src = `${birdsData[indexBirdsData][i].image}`
     }
 
-    const describeText = document.querySelector('.describe-bird p')    
+    const describeText = document.querySelector('.describe-bird p')
     describeText.textContent = `${birdsData[indexBirdsData][i].description}`
 
     const describeAudio = document.querySelector('.describe-bird audio')
     describeAudio.src = `${birdsData[indexBirdsData][i].audio}`
+
+    objectAnswer.answer = `${birdsData[indexBirdsData][i].name}`
+
+    isTrueAnswer()
 }
 
 // fullscreen
@@ -184,4 +197,54 @@ function preloadImg(){
             img.onload = () => imgArr.push(img.src)
         }
     }
+}
+// загадываем птицу
+function guessBird(i){
+    const guessBird = randomBirds(i) // получили птицу, которую нужно загадать
+    console.log(guessBird)
+
+    const taskAudio = document.querySelector('.task-block audio')
+    taskAudio.src = `${guessBird.audio}`
+
+    objectAnswer.guess = guessBird.name
+}
+
+// рандомно выбираем из массива
+function randomBirds(i){
+    const shuffleArr = birdsData[i]
+    const numberArr = [0, 1, 2, 3, 4, 5]
+    shuffleArr.sort(()=> Math.random() - 0.5)
+    numberArr.sort(()=> Math.random() - 0.5)
+    return shuffleArr[numberArr[0]]
+}
+
+function isTrueAnswer(){
+
+    if(objectAnswer.guess == objectAnswer.answer){
+        taskBirdsNamesMarkers.forEach(el =>{
+            if(el.firstElementChild.innerText == objectAnswer.answer){
+                el.classList.add('trueAnswer')
+            }
+        })
+
+    } else{
+        taskBirdsNamesMarkers.forEach(el =>{
+            if(el.firstElementChild.innerText == objectAnswer.answer){
+                el.classList.add('falseAnswer')
+            }
+        })
+    }
+}
+// убираем цвет а маркерах
+function checkMarkerClass(){
+    console.log('!')
+    taskBirdsNamesMarkers.forEach(el =>{
+        console.log(el.classList)
+        if(el.classList.contains('trueAnswer')){
+            el.classList.remove('trueAnswer')
+        }
+        if(el.classList.contains('falseAnswer')){
+            el.classList.remove('falseAnswer')
+        }
+    })
 }
